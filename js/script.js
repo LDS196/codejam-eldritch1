88339dc678848b931bd2be85ce80da446af8b72d
LDS196 <lihachevd87@gmail.com>
2022-08-29 23:18:59 +0400

    feat: add choise ancients in script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ancientsData from '../data/ancients.js';
-import difficulties from '../data/difficulties.js';
+//import difficulties from '../data/difficulties.js';
 import greencardsData from '../data/mythicCards/green/index.js';
 import browncardsData from '../data/mythicCards/brown/index.js';
 import bluecardsData from '../data/mythicCards/blue/index.js';
@@ -8,7 +8,7 @@ import bluecardsData from '../data/mythicCards/blue/index.js';
 //console.log(browncardsData)
 //console.log(bluecardsData)
 
-
+let numAcients;
  let playCardsSrc;
 let playCards = [];
 
@@ -42,19 +42,23 @@ const greenPlayCards = [];
 const brownPlayCards = [];
 const bluePlayCards = [];
 
-const sumGreenCards = +ancientsData[0].firstStage.greenCards + +ancientsData[0].secondStage.greenCards + +ancientsData[0].thirdStage.greenCards
-const sumBrownCards = +ancientsData[1].firstStage.brownCards + +ancientsData[1].secondStage.brownCards + +ancientsData[1].thirdStage.brownCards
-const sumBlueCards = +ancientsData[1].firstStage.blueCards + +ancientsData[1].secondStage.blueCards + +ancientsData[1].thirdStage.blueCards
+const sumGreenCards = +ancientsData[numAcients].firstStage.greenCards + +ancientsData[numAcients].secondStage.greenCards + +ancientsData[numAcients].thirdStage.greenCards
+const sumBrownCards = +ancientsData[numAcients].firstStage.brownCards + +ancientsData[numAcients].secondStage.brownCards + +ancientsData[numAcients].thirdStage.brownCards
+const sumBlueCards = +ancientsData[numAcients].firstStage.blueCards + +ancientsData[numAcients].secondStage.blueCards + +ancientsData[numAcients].thirdStage.blueCards
+
+console.log(sumGreenCards) 
+console.log(sumBrownCards) 
+console.log(sumBlueCards )
+
 
 repeat(greenPlayCards, greencardsData, sumGreenCards);
 repeat(brownPlayCards, browncardsData, sumBrownCards);
 repeat(bluePlayCards, bluecardsData, sumBlueCards);
 
-
 //
-const sumFirstStageGreenCards = ancientsData[0].firstStage.greenCards;
-const sumFirstStageBrownCards = ancientsData[0].firstStage.brownCards;
-const sumFirstStageBlueCards = ancientsData[0].firstStage.blueCards;
+const sumFirstStageGreenCards = ancientsData[numAcients].firstStage.greenCards;
+const sumFirstStageBrownCards = ancientsData[numAcients].firstStage.brownCards;
+const sumFirstStageBlueCards = ancientsData[numAcients].firstStage.blueCards;
 
 const firstStageGreen = [];
 const firstStageBrown = [];
@@ -63,13 +67,10 @@ const firstStageBlue = [];
 repeat1(firstStageGreen, greenPlayCards, sumFirstStageGreenCards);
 repeat1(firstStageBrown, brownPlayCards, sumFirstStageBrownCards);
 repeat1(firstStageBlue, bluePlayCards, sumFirstStageBlueCards);
-
-
-
 //
-const sumSecondStageGreenCards = ancientsData[0].secondStage.greenCards;
-const sumSecondStageBrownCards = ancientsData[0].secondStage.brownCards;
-const sumSecondStageBlueCards = ancientsData[0].secondStage.blueCards;
+const sumSecondStageGreenCards = ancientsData[numAcients].secondStage.greenCards;
+const sumSecondStageBrownCards = ancientsData[numAcients].secondStage.brownCards;
+const sumSecondStageBlueCards = ancientsData[numAcients].secondStage.blueCards;
 
 const secondStageGreen = []
 const secondStageBrown = []
@@ -79,13 +80,10 @@ repeat1(secondStageGreen, greenPlayCards, sumSecondStageGreenCards);
 repeat1(secondStageBrown, brownPlayCards, sumSecondStageBrownCards);
 repeat1(secondStageBlue, bluePlayCards, sumSecondStageBlueCards);
 
-
-
-
 //
-const sumThirdStageGreenCards = ancientsData[0].thirdStage.greenCards;
-const sumThirdStageBrownCards = ancientsData[0].thirdStage.brownCards;
-const sumThirdStageBlueCards = ancientsData[0].thirdStage.blueCards;
+const sumThirdStageGreenCards = ancientsData[numAcients].thirdStage.greenCards;
+const sumThirdStageBrownCards = ancientsData[numAcients].thirdStage.brownCards;
+const sumThirdStageBlueCards = ancientsData[numAcients].thirdStage.blueCards;
 
 const thirdStageGreen = []
 const thirdStageBrown = []
@@ -109,10 +107,6 @@ const thirdStageCards = thirdStageGreen.concat(thirdStageBrown, thirdStageBlue)
    playCards.splice(-1, 0, ...playCards.splice(-1, 1, secondStageMix))
    playCards.splice(-1, 0, ...playCards.splice(-1, 1, thirdStageMix))
    playCardsSrc = firstStageMix.concat(secondStageMix, thirdStageMix)
-   
-  
-
-
 currentCardPlay.src = " ";
 
 treker();
@@ -123,39 +117,53 @@ const currentCardPlay  = document.querySelector('.current-card');
 
 function showCard(){
    if ( playCards[0].length != 0){
-      currentCardPlay.src = playCards[0][0].cardFace;
-   //console.log(playCards[0])
+   currentCardPlay.src = playCards[0][0].cardFace;
    playCards[0].splice(0, 1);
    treker();
-   //console.log(playCards[0])
    } else if (playCards[1].length != 0){
       currentCardPlay.src = playCards[1][0].cardFace;
-     // console.log(playCards[1])
       playCards[1].splice(0, 1);
       treker();
-     // console.log(playCards[1])
+     
    } else if (playCards[2].length != 0){
       currentCardPlay.src = playCards[2][0].cardFace;
       //console.log(playCards[2])
       playCards[2].splice(0, 1);
       treker();
-      //console.log(playCards[2])
+    
    } else deck.classList.remove('deck-active');
-     
-   
-
-
 };
 deck.addEventListener('click', showCard );
 //
-const ancientsImg = document.querySelector('.ancients_img');
+const ancientsImg = document.querySelectorAll('.ancients_img');
 const difficultiesButton = document.querySelector('.difficulties_button');
 const shuffleButton = document.querySelector('.shuffle_button');
-
-ancientsImg.addEventListener('click', () => {
+/*
+   ancientsImg.addEventListener('click', () => {
    ancientsImg.classList.add('ancients_img-active');
-   difficultiesButton.classList.add('difficulties_button-active')
+      difficultiesButton.classList.add('difficulties_button-active')
 });
+*/
+
+ancientsImg.forEach(function(btn){
+   btn.addEventListener('click', () => {
+      btn.classList.add('ancients_img-active');
+      difficultiesButton.classList.add('difficulties_button-active');
+      if(btn.alt == 'Azathoth' ){
+         numAcients = 0
+      } else if (btn.alt == 'Cthulthu'){
+         numAcients = 1
+      } else if (btn.alt == 'IogSothoth'){
+         numAcients = 2
+      } else if  (btn.alt == 'ShubNiggurath'){
+         numAcients = 3
+      }
+   });
+})
+
+
+
+
 
 difficultiesButton.addEventListener('click', () => {
    shuffleButton.classList.add('shuffle_button-active')
@@ -164,13 +172,9 @@ difficultiesButton.addEventListener('click', () => {
 shuffleButton.addEventListener('click', () =>{
    shuffleCards();
    deck.classList.add('deck-active');
-   //currentCardPlay.classList.remove('.current-card-active')
 })
 
-
-
 function treker(){
-
    const dot = document.querySelectorAll('.dot');
    dot[0].textContent = playCards[0].filter( card => card.color === 'green').length;
    dot[1].textContent = playCards[0].filter( card => card.color === 'brown').length;
